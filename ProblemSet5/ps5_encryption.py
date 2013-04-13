c25515ebee68335e1565365be2566279a18e862b
unknown <merisw@hotmail.com>
2013-04-13 00:56:54 -0700

    Encrypt and decrypt, Caesar cypher

---
+++ ProblemSet5/ps5_encryption.py
@@ -0,0 +1,207 @@
+# 6.00x Problem Set 5
+#
+# Part 1 - HAIL CAESAR!
+
+import string
+import random
+
+WORDLIST_FILENAME = "words.txt"
+
+# -----------------------------------
+# Helper code
+# (you don't need to understand this helper code)
+def loadWords():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    inFile = open(WORDLIST_FILENAME, 'r')
+    wordList = inFile.read().split()
+    print "  ", len(wordList), "words loaded."
+    return wordList
+
+def isWord(wordList, word):
+    """
+    Determines if word is a valid word.
+
+    wordList: list of words in the dictionary.
+    word: a possible word.
+    returns True if word is in wordList.
+
+    Example:
+    >>> isWord(wordList, 'bat') returns
+    True
+    >>> isWord(wordList, 'asdf') returns
+    False
+    """
+    word = word.lower()
+    word = word.strip(" !@#$%^&*()-_+={}[]|\\:;'<>?,./\"")
+    return word in wordList
+
+def randomWord(wordList):
+    """
+    Returns a random word.
+
+    wordList: list of words  
+    returns: a word from wordList at random
+    """
+    return random.choice(wordList)
+
+def randomString(wordList, n):
+    """
+    Returns a string containing n random words from wordList
+
+    wordList: list of words
+    returns: a string of random words separated by spaces.
+    """
+    return " ".join([randomWord(wordList) for _ in range(n)])
+
+def randomScrambled(wordList, n):
+    """
+    Generates a test string by generating an n-word random string
+    and encrypting it with a sequence of random shifts.
+
+    wordList: list of words
+    n: number of random words to generate and scamble
+    returns: a scrambled string of n random words
+
+    NOTE:
+    This function will ONLY work once you have completed your
+    implementation of applyShifts!
+    """
+    s = randomString(wordList, n) + " "
+    shifts = [(i, random.randint(0, 25)) for i in range(len(s)) if s[i-1] == ' ']
+    return applyShifts(s, shifts)[:-1]
+
+def getStoryString():
+    """
+    Returns a story in encrypted text.
+    """
+    return open("story.txt", "r").read()
+
+
+# (end of helper code)
+# -----------------------------------
+
+
+#
+# Problem 1: Encryption
+#
+def buildCoder(shift):
+    """
+    Returns a dict that can apply a Caesar cipher to a letter.
+    The cipher is defined by the shift value. Ignores non-letter characters
+    like punctuation, numbers and spaces.
+
+    shift: 0 <= int < 26
+    returns: dict
+    """
+    alph = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10,
+            'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19,
+            't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
+    alphUpper = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10,
+                 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19,
+                 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
+    code = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', 10:'j',
+             11:'k', 12:'l', 13:'m', 14:'n', 15:'o', 16:'p', 17:'q', 18:'r', 19:'s',
+             20:'t', 21:'u', 22:'v', 23:'w', 24:'x', 25:'y', 26:'z'}
+    codeUpper = {1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J',
+                 11:'K', 12:'L',13:'M', 14:'N', 15:'O', 16:'P', 17:'Q', 18:'R', 19:'S',
+                 20:'T', 21:'U', 22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
+
+    newAlph = alph.copy()
+    newAlphUpper = alphUpper.copy()
+    for letter in alph:
+        start = alph[letter]
+        move = start + shift
+        if move > 26:
+            move = move - 26
+        newAlph[letter] = code[move]
+        Letter = letter.upper()
+        newAlphUpper[Letter] = codeUpper[move]
+    newAlph.update(newAlphUpper)
+    return newAlph
+                
+def applyCoder(text, coder):
+    """
+    Applies the coder to the text. Returns the encoded text.
+
+    text: string
+    coder: dict with mappings of characters to shifted characters
+    returns: text after mapping coder chars to original text
+    """
+    output = ''
+    for c in text:
+        if c.isalpha():
+            output += coder[c]
+        else:
+            output += c
+    return output
+
+def applyShift(text, shift):
+    """
+    Given a text, returns a new text Caesar shifted by the given shift
+    offset. Lower case letters should remain lower case, upper case
+    letters should remain upper case, and all other punctuation should
+    stay as it is.
+
+    text: string to apply the shift to
+    shift: amount to shift the text (0 <= int < 26)
+    returns: text after being shifted by specified amount.
+    """
+    
+    return applyCoder(text, buildCoder(shift)) 
+
+#
+# Problem 2: Decryption
+#
+def findBestShift(wordList, text):
+    """
+    Finds a shift key that can decrypt the encoded text.
+
+    text: string
+    returns: 0 <= int < 26
+    """
+    maxWords = 0
+    bestShift = 0
+    for shift in range(27):
+        message = applyShift(text, shift)
+        words = message.split(' ')       
+        validWords = 0
+        for word in words[:]:
+            if isWord(wordList, word):
+                validWords += 1
+        if validWords > maxWords:
+            maxWords = validWords
+            bestShift = shift
+    return bestShift
+
+def decryptStory():
+    """
+    Using the methods you created in this problem set,
+    decrypt the story given by the function getStoryString().
+    Use the functions getStoryString and loadWords to get the
+    raw data you need.
+
+    returns: string - story in plain text
+    """
+    wordList = loadWords()
+    text = getStoryString()
+    shift = findBestShift(wordList, text)
+    return applyShift(text, shift)
+    
+#
+# Build data structures used for entire session and run encryption
+#
+
+if __name__ == '__main__':
+    # To test findBestShift:
+    #wordList = loadWords()
+    #s = applyShift('Hello, world!', 8)
+    #bestShift = findBestShift(wordList, s)
+    #assert applyShift(s, bestShift) == 'Hello, world!'
+    # To test decryptStory, comment the above four lines and uncomment this line:
+        decryptStory()
